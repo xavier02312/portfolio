6ddb88b5e06d72626221870424443ad770aa62fd
xavier02312 <paqueriaudxavier@gmail.com>
2022-04-24 12:58:41 +0200

    continu

---
+++ fonction/fonction.js
@@ -0,0 +1,50 @@
+/**
+ * Fonction
+ * Formulaire
+ */
+//Défini si mon champ et vide ou pas
+const notEmpty = (value) => {
+
+    if (typeof value !== "string" || value.length === 0)
+    return false;
+
+    else{
+        return true;
+    }
+};
+//Fonction d'erreur message
+
+const isError = (message, success, id) => {
+
+    //Récupère le champ input
+    const input = document.querySelector(`#${id}`);
+
+    //Récupère la balise contenant le message d'erreur
+    const messageError = document.querySelector(`#${id}Error`);
+
+    //Affiche le message d'erreur
+    input.style.border = "2px solid red";
+    messageError.innerText = message;
+    messageError.style.display = "block";
+    
+    //affiche un succès
+    if (success) {
+        input.style.border = "2px solid green";
+        messageError.style.display = "none";
+    }
+ };
+
+ //Fonction de e-mail
+const isEmail = (value) => {
+
+    //code pattern vérif de caractère
+    const pattern =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return pattern.test(value);
+};
+
+//Longueur de chaîne de caractère
+const isLength = (value, min) => {
+    return value.length >= min;
+}
+
+
--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 /**
  * Chargement du DOM
  */
-document.addEventListener("DOMContentLoaded", function() {
+window.onload = function() {
     alert("Document ready!");
-});
-
+}
 /**
  * 
  * 
@@ -12,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * 
  * 
  */
-const open = document.getElementById("open");         //1element//
-const burger = document.getElementById("burger");     //2element//
-const ul = document.querySelector("ul");              //3element//
+const open = document.getElementById("open");         // 1élément//
+const burger = document.getElementById("burger");     // 2élément//
+const ul = document.querySelector("ul");              // 3élément//
 
 open.addEventListener("click", function(event) {
     event.preventDefault()
@@ -24,9 +23,66 @@ open.addEventListener("click", function(event) {
 /**
  * Formulaire
  * champs Nom
- * 
  */
+//Le nom
+//Séléctionne le champ "input" text
+ const lastName = document.querySelector("#lastname");
+
+ //Applique un écouteur d'évènement
+lastName.addEventListener('input', () => {
+
+    //Vérifie si le champs est vide 
+    const response = notEmpty(lastName.value);
+
+    //Fonction d'erreur
+    isError("Votre nom est obligatoire", response, "lastname");
+
+});
+//Le prenom
+const firstname = document.querySelector("#firstname");
+
+//Ecouteur d'évènement
+firstname.addEventListener("input", () => {
+
+    //Vérifie le champs
+    const response = notEmpty(firstname.value);
+
+    //Fonction d'erreur
+    isError("Votre prénom est obligatoire" , response, "firstname");
+    
+});
+//Le Mail
+const mail = document.querySelector("#email");
+
+//Ecouteur d'évènement
+mail.addEventListener("input", () => {
+
+    //Vérifie le champs
+    const response = notEmpty(mail.value);
+
+    //Fonction d'erreur
+    isError("Votre Adresse e-mail est obligatoire", response, "email");
+
+    //fonction mail
+    if (response) {
+        
+        const checkEmail = isEmail(mail.value);
+        isError("L'adresse e-mail est invalide", checkEmail, "email");
+    }
 
 
+});
+//Le téléphone
+const telephone = document.querySelector("#phone");
+
+//Ecouteur d'événement
+telephone.addEventListener("input", () => {
+
+    //Vérifie le champs
+    const response = notEmpty(telephone.value);
+
+    //Fonction d'erreur
+    
+});
     
     
